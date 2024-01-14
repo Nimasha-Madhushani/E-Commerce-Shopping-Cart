d268b518fe9e89e1ee16602287199e7a48d14b60
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2024-01-14 10:28:42 +0530

    create the project structure

---
+++ client/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+import TopNavBar from "../components/Header/TopNavBar";
+import Footer from "../components/Footer/Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Fragment>
+        <TopNavBar />
+        <Footer />
+      </Fragment>
+    </div>
+  );
+};
+
+export default Home;
